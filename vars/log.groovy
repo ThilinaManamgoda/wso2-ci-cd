d37567ad7c455d1543bebfae1a57b1605dca0ce3
ThilinaManamgoda <maanafunedu@gmail.com>
2018-11-27 12:25:02 +0530

    Change ANSI color codes

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,12 +1,12 @@
 
 def err(msg) {
     ansiColor('xterm') {
-        echo "\u001B[36mERROR: $msg"
+        echo "\u001B[31mERROR: $msg"
     }
 }
 def info(msg) {
     ansiColor('xterm') {
-        echo "\u001B[32mINFO: $msg"
+        echo "\u001B[36mINFO: $msg"
     }
 
 }
