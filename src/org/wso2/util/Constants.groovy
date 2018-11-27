b46f2b05d769bdde4c3f05b0f23f40adf73ce5d4
ThilinaManamgoda <maanafunedu@gmail.com>
2018-11-27 12:12:37 +0530

    Add Constants

--- src/org/wso2/util/Constants.groovy
+++ src/org/wso2/util/Constants.groovy
@@ -4,7 +4,7 @@ package org.wso2.util
  * This class holds constants
  */
 class Constants {
-    static class ControlConstants {
-        final int STATUS_COMPLETED = 0
+    class ControlConstants {
+        static final int STATUS_COMPLETED = 0
     }
 }
